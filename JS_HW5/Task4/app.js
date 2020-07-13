83e19d896028dd6ed682188f4d28aa50c4fe485e
mitant127 <55878970+mitant127@users.noreply.github.com>
2020-07-13 11:48:02 +0300

    Add task5
    Сделал 5-е задание. Добавил заготовку для расстановки фигур.
    4-е поправил. Но думаю должно быть лучшее решение чем обращение по номеру элемента массива. Массив может измениться тогда скрипт перестанет работать.

--- JS_HW5/Task4/app.js
+++ JS_HW5/Task4/app.js
@@ -6,7 +6,7 @@ const buttons = document.querySelectorAll('button');
 
 // 1.1 затем проитерируйтесь по кнопкам и каждой из
 // них добавьте обработчик клика - функцию handleClick
-buttons.forEach(function(button) {
+buttons.forEach(function (button) {
     button.addEventListener('click', handleClick);
 });
 
@@ -16,31 +16,28 @@ buttons.forEach(function(button) {
  * @param {MouseEvent} clickedButtonEvent 
  */
 function handleClick(clickedButtonEvent) {
-//        console.log('click');
-//        console.log(clickedButtonEvent);
-//        console.log(clickedButtonEvent.path[1]);
+    //        console.log(clickedButtonEvent);
+    //        console.log(clickedButtonEvent.path.);
 
     // 2. из объекта события получите ссылку на .product и
     // сохраните в переменную:
-     const cardNode = clickedButtonEvent.path[1];
-//    console.log(cardNode);
-    
+    const cardNode = clickedButtonEvent.path[1];
+    //    console.log(cardNode.children[cardNode.children.length - 1]);
+
     // 3. создайте литерал объекта со следующими свойствами:
     const card = {
         wrap: cardNode, // здесь элемент с классом .product
         img: cardNode.children[1], // здесь картинка внутри .product
         productName: cardNode.children[0], // здесь .productName, который внутри .product
-        button: cardNode.children[2], // здесь button, который внутри .product
+        button: cardNode.children[cardNode.children.length - 1], // здесь button, который внутри .product
     };
 
-    console.log(card);
-    
     // 4. получаем текст на кнопке, которая внутри .product
-    
+
     let buttonText = card.button.innerText
-    
+
     console.log(buttonText);
-    
+
     if (buttonText == 'Подробнее') { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
         // 4.2 если да, то передаем объект card в функцию showMoreText
         showMoreText(card);
@@ -62,15 +59,21 @@ function hideMoreText(card) {
     // 5. картинке внутри .product ставим стиль display: block
 
     card.img.style.display = "block";
-    
+
     // 5.1 внутри .product находим элемент с классом .desc и удаляем его
+
+    //=============================================
+    // мне кажется тут должно быть более элегантное решение чем обращение по номеру элемента массива
+    // также и в "const card"
+    // но как это сделать не разобрался
+    //=============================================
     
-    //    card.classList.remove("desc");
-    
+    card.wrap.childNodes[5].remove();
+
     // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
-    
+
     card.button.innerText = 'Подробнее';
-    
+
 }
 
 /**
@@ -85,23 +88,21 @@ function showMoreText(card) {
     // 6. картинке внутри .product ставим display: none
 
     card.img.style.display = "none";
-    
+
     // 6.1 сохраняем произвольный текст в переменную
-    
+
     const descText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea obcaecati sed eos eveniet ipsum itaque aut vitae illo molestias eligendi.';
-    
+
     // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1
-    
+
     let product = card.wrap.querySelector('.productName').parentNode;
-//    console.log(product);
     let childElem = product.querySelectorAll("button");
-//     console.log(childElem);
     let dive = document.createElement('div');
     dive.className = "desc";
     product.insertBefore(dive, childElem[0]);
     dive.innerText = descText;
-    
+
     // 6.3 внутри .product у кнопки текст ставим "Отмена"
-    
+
     card.button.innerText = 'Отмена';
 }
